b6489fbd6ac99aa3ba84abd42e9edcef60c7462e
shiftbyjeanduplessis <shiftbyjeanduplessis@gmail.com>
2026-06-18 14:52:06 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hearty-production-hotfix-v7-data-recovery";
+const CACHE_NAME = "hearty-production-hotfix-v10-nav-visible";
 self.addEventListener("install", event => { self.skipWaiting(); });
 self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
