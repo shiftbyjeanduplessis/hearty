9fc28968ee4bd1576ccb5f13e41e9c6dbf6d9b6f
shiftbyjeanduplessis <shiftbyjeanduplessis@gmail.com>
2026-06-18 13:15:08 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hearty-production-hotfix-v5";
+const CACHE_NAME = "hearty-production-hotfix-v6-howto-header";
 self.addEventListener("install", (event) => { self.skipWaiting(); });
 self.addEventListener("activate", (event) => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()))
