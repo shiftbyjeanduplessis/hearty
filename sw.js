47ab28b9768a453950b55cbee23aca30168be7c3
shiftbyjeanduplessis <shiftbyjeanduplessis@gmail.com>
2026-06-18 13:20:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "hearty-production-hotfix-v6-howto-header";
-self.addEventListener("install", (event) => { self.skipWaiting(); });
-self.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()))
+const CACHE_NAME = "hearty-production-hotfix-v7-data-recovery";
+self.addEventListener("install", event => { self.skipWaiting(); });
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(fetch(event.request, { cache: "no-store" }).catch(() => caches.match(event.request)));
 });
